0db44b8ef2a352f3342a6d59e0df2350bc965cdf
Gabriel Castillo <trodado@uninorte.edu.co>
2023-05-24 18:42:40 -0500

    struture and css

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from "./components/header/Header.jsx"
+import Nav from "./components/nav/Nav.jsx"
+import About from "./components/about/About.jsx"
+import Services from "./components/services/Services.jsx"
+import Portfolio from "./components/portfolio/Portfolio.jsx"
+import Contact from "./components/contacts/Contact.jsx"
+import Footer from "./components/footer/Footer.jsx"
+import Experience from './components/experience/Experience.jsx'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
