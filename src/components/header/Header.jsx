187b7bffd029f0b45336c8b015716c77ee23d523
Gabriel Castillo <trodado@uninorte.edu.co>
2023-05-28 14:01:54 -0500

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import "./Header.css"
+import CTA from "./CTA.jsx"
+import ME from "../../assets/me.png"
+import HeaderSocials from './HeaderSocials'
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Gabriel Castillo</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
